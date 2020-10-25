5ac8c4a6b973b585e4fcc2f428c83c4c71e1581d
mfathirirhas <mfathirirhas@gmail.com>
2020-10-26 06:35:34 +0700

    Change package name to httpserver

--- server_unix.go
+++ server_unix.go
@@ -1,6 +1,6 @@
 // +build darwin linux freebsd openbsd netbsd
 
-package server
+package httpserver
 
 import (
 	"fmt"
--- server_windows.go
+++ server_windows.go
@@ -1,6 +1,6 @@
 // +build windows
 
-package server
+package httpserver
 
 import (
 	"fmt"
