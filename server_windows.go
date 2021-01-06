efc11c44bf0047b37cd93b91b6a82b5e4194d4c2
mfathirirhas <mfathirirhas@gmail.com>
2021-01-06 07:27:29 +0700

    Add not found handler in windows server

--- server_windows.go
+++ server_windows.go
@@ -10,11 +10,18 @@ import (
 
 // graceful is not support in Windows. Using built-in package instead. This is for avoiding this package failed to run locally, rarely Windows used in server now.
 func (s *Server) serve() error {
+	var handler http.Handler = s.handlers
+	if s.cors != nil {
+		handler = s.cors.Handler(s.handlers)
+	}
 	var tlsConfig *tls.Config
 	tlsConfig = s.tls
+	if s.notFoundHandler != nil {
+		s.handlers.NotFound = s.notFoundHandler
+	}
 	srv := &http.Server{
 		Addr:        fmt.Sprintf(":%d", s.port),
-		Handler:     s.cors.Handler(s.handlers),
+		Handler:     handler,
 		IdleTimeout: s.idleTimeout,
 		TLSConfig:   tlsConfig,
 	}
